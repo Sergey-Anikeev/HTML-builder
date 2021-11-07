5a41d1f409073fa253f4a63e9b30f9d8b0452ab9
Sergey_Anikeev <serega.anikeev2016@gmail.com>
2021-11-07 17:10:05 +0300

    feat: 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,63 @@
+//import required modules
+const fs = require('fs');
+const path = require('path');
+// const { readdir } = require('fs/promises');
+// const stats = require('fs');
+
+//read folder content
+
+
+
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true}, (err, files) => {
+    for (const file of files) {      
+        let fileSize;    
+        if (!file.isDirectory()) {           
+            fs.stat(path.join(path.join(__dirname, 'secret-folder'), file.name), (err, stats) => {
+                fileSize = stats.size;
+                console.log(fileSize);
+            });
+            console.log(`${file.name.split('.')[0]} - ${path.extname(file.name).slice(1)} - ${fileSize}b`); //${fileSizeArr.at(files.indexOf(file))}
+        }   
+    }
+    console.log('files into: ', files);
+});
+
+// const files = readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true});
+// console.log('files into: ', files);
+
+// (async function (pat) {
+//     try {
+//         const files = await readdir(pat, { withFileTypes: true});
+        
+//         console.log('files into: ', files);
+
+//         let fileSizeArr = [];
+//         console.log('fileSizeArr created: ', fileSizeArr);
+
+//         for (const file of files) {
+//             fs.stat(path.join(path.join(__dirname, 'secret-folder'), file.name), (err, stats) => {
+//                 if(stats.isFile()) {
+//                     fileSizeArr.push(stats.size);
+//                     console.log(stats.size);
+//                 }
+//             });
+//         }
+//         console.log('fileSizeArr after: ', fileSizeArr);
+//     } catch (error) {
+//         console.error('there was an error: ', error.message);
+//     } 
+// })(path.join(__dirname, 'secret-folder'));
+
+
+// for (const file of files) {          
+//     if (!file.isDirectory()) {
+//         console.log(`${file.name.split('.')[0]} - ${path.extname(file.name).slice(1)} - ${fileSizeArr.at(files.indexOf(file))}b`);
+//     }    
+// }
+
+
+//collect folder objects data
+
+//file checking
+
+//console output
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+// modules import
+const path = required('path');
+const fs = required('fs');
+const copyFile = required('fs/promises');
 
+// create file-copy if isn't exist// fsPromises/mkdir {recursive: }
+try {
+    await copyFile('')
+} catch {
+
+}
+
+
+// files  content read
+
+// copy files
